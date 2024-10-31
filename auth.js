c90bdef446e4edc8cbc2e2de03364ded8a94e798
zuitt-cst <cst@tuitt.com>
2024-10-31 13:35:43 +0800

    updated

--- auth.js
+++ auth.js
@@ -38,3 +38,32 @@ const secret = "fitnessApp";
             })
         }
     };
+
+    module.exports.verify = (req, res, next) => {
+        // console.log(req.headers.authorization);
+    
+        let token = req.headers.authorization;
+    
+        if(typeof token === "undefined"){
+            return res.send({ auth: "Failed. No Token" });
+        } else {
+            // console.log(token);		
+            token = token.slice(7, token.length);
+            // console.log(token);
+    
+            jwt.verify(token, secret, function(err, decodedToken){ 
+                if(err){
+                    return res.send({
+                        auth: "Failed",
+                        message: err.message
+                    });
+                } else {
+                    // console.log("result from verify method:")
+                    // console.log(decodedToken);
+                    
+                    req.user = decodedToken;
+                    next();
+                }
+            })
+        }
+    };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/user");
+const {verify} = require("../auth");
 
 const router = express.Router();
 
